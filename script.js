95263394dd42320b4a3c76f221468ee537bf4028
Teodor <gra4ev.teodor@yandex.ru>
2024-06-19 13:37:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function greetUser() {
+    const name = document.getElementById('nameInput').value;
+    const message = name ? `Привет, ${name}!` : 'Пожалуйста, введите ваше имя.';
+    document.getElementById('greetingMessage').innerText = message;
+}

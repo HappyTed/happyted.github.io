47baa0f58fc15a1bef4c818390c68ce519f56a33
Teodor <gra4ev.teodor@yandex.ru>
2024-07-15 15:13:30 +0300

    add push subscribe button

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function greetUser() {
     const name = document.getElementById('nameInput').value;
     const message = name ? `Привет, ${name}!` : 'Пожалуйста, введите ваше имя.';
     document.getElementById('greetingMessage').innerText = message;
+
+    return name
 }
 
 function _(){
